fb9e3975c8b38c8f7c85fa2b1424482584209a55
Yin Deng <yindeng@berkeley.edu>
2021-01-05 14:39:34 -0800

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ $(document).on('click','.navbar-collapse.in',function(e) {
         $(this).collapse('hide');
     }
 });
+
+// Source: https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
+$(document).ready(function(){
+    $('body').scrollspy({target: ".navbar", offset: 50});
+    $("#myNavbar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+            window.location.hash = hash;
+            });
+        } 
+    });
+});
